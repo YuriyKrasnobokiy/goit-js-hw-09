709ce4da6274a890c14ede501f0e3f4fc83b0b64
Yuriy Krasnobokiy <krasnobokiy.yuriy@gmail.com>
2023-08-17 20:15:59 +0300

    add 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timer = null;
+startBtn.addEventListener('click', handlerOnStart);
+stopBtn.addEventListener('click', handlerOnStop);
 
+function handlerOnStart() {
+  timer = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+}
+
+function handlerOnStop() {
+  clearInterval(timer);
+  startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
